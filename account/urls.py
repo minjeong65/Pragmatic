e3659183093cfe66a6618e66c90d847708744ea5
minjeong65 <aplus930@gmail.com>
2021-12-10 23:25:29 +0900

    first

---
+++ account/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from account.views import hello_world
+
+app_name = "account"
+
+urlpatterns = [
+    path('hello/', hello_world, name = "hello_world"),
+]
